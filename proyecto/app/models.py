e400a21cdb494ec100036bb0ece592638c368bd6
DCueto <dcueto14.info@gmail.com>
2014-10-23 13:06:47 +0200

    Init models and database

--- proyecto/app/models.py
+++ proyecto/app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Categoria(models.Model):
+	titulo = models.CharField(max_length = 140)
+
+class Enlace(models.Model):
+	titulo = models.CharField(max_length = 140)
+	enlace = models.URLField()
+	votos = models.IntegerField(default=0)
