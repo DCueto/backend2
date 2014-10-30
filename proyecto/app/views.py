e7085dadad22428f17a1f12ed2dd5bd67b2fcfb5
DCueto <dcueto14.info@gmail.com>
2014-10-30 13:58:26 +0100

    Forms

--- proyecto/app/views.py
+++ proyecto/app/views.py
@@ -1,11 +1,57 @@
 from django.shortcuts import render
 from django.shortcuts import render_to_response
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template.loader import get_template
 from django.template import Context
 from datetime import datetime
+from models import *
+from django.shortcuts import get_object_or_404
+from forms import *
+from django.template.context import RequestContext
 
 # Create your views here.
+
+def home(request):
+	categorias = Categoria.objects.all()
+	enlaces = Enlace.objects.all()
+	template = "index.html"
+
+	return render_to_response(template, locals())
+
+def categoria(request, id_categoria):
+	categorias = Categoria.objects.all()
+	cat = get_object_or_404(Categoria, pk = id_categoria)
+	# cat = Categoria.objects.get(pk = id_categoria)
+	enlaces = Enlace.objects.filter(categoria = cat)
+	template = "index.html"
+
+	return render_to_response(template, locals())
+
+
+def minus(request, id_enlace):
+	enlace = Enlace.objects.get(pk=id_enlace)
+	enlace.votos = enlace.votos - 1
+	enlace.save()
+	return HttpResponseRedirect("/")
+
+def plus(request, id_enlace):
+	enlace = Enlace.objects.get(pk=id_enlace)
+	enlace.votos = enlace.votos + 1
+	enlace.save()
+	return HttpResponseRedirect("/")
+
+def add(request):
+	categorias = Categoria.objects.all()
+	if request.method == "POST":
+		form = EnlaceForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return HttpResponseRedirect("/")
+	else:
+		form = EnlaceForm()
+
+	template = "form.html"
+	return render_to_response(template, context_instance = RequestContext(request,locals()))
 
 def hora_actual(request):
 
